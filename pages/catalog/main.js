8a19d7b8b97a1d8de9e5f44c1ae52ca214e4cb3a
BeataMaro <beatamaro@interia.pl>
2022-11-13 00:26:36 +0100

    feat: create book cards

--- pages/catalog/main.js
+++ pages/catalog/main.js
@@ -1,3 +1,18 @@
-fetch("../../books.json")
+const body = document.body;
+
+const getBooks = async () => {
+ let books = await fetch("../../books.json")
   .then((res) => res.json())
-  .then((data) => console.log(data));
+  .then((data) => data.map(b => {
+    const bookTile = document.createElement('article');
+    const bookTileHeader = document.createElement('header');
+    const author = document.createElement('h3');
+    author.textContent = b.author;
+    bookTileHeader.appendChild(author); 
+    bookTile.appendChild(bookTileHeader);
+    bookTile.classList.add("book-tile")
+    body.appendChild(bookTile)
+  }));
+}
+
+getBooks();
