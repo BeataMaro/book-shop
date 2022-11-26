2798e1c275436000129ed58c18290529ea6adf53
BeataMaro <beatamaro@interia.pl>
2022-11-26 23:48:34 +0100

    fix: hide error message when form data is valid

--- pages/order-page/main.js
+++ pages/order-page/main.js
@@ -3,18 +3,31 @@
 // getHeader();
 
 const deliveryForm = document.forms["delivery-form"]; //document.forms[0] or document.forms.deliveryForm
+
+const submitButton = document.getElementById("button-send-form");
 //form data
 const firstname = deliveryForm["firstname"].value;
 const surname = deliveryForm["surname"];
 const dateInput = deliveryForm["delivery-date"];
+const submittedData = {};
 
-console.log(dateInput.form);
+// console.log(dateInput.form);
 
 deliveryForm.onsubmit = (e) => {
   e.preventDefault();
+  submittedData.name = deliveryForm.firstname.value;
+  submittedData.surname = deliveryForm.surname.value;
+  submittedData.street = deliveryForm.street.value;
+  submittedData.houseNumber = deliveryForm.houseNumber.value;
+  submittedData.flatNumber = deliveryForm.flatNumber.value;
+  submittedData.deliveryDate = deliveryForm["delivery-date"].value;
+  submittedData.paymentMethod = deliveryForm["payment-method"].value;
+  submittedData.gifts = [...deliveryForm.gift.value];
 
-  console.log(deliveryForm.surname.value); //deliveryForm.elements.surname.value
+  console.log(submittedData); //deliveryForm.elements.surname.value
 };
+
+const validateForm = () => {};
 
 const showSummary = () => {};
 
@@ -30,3 +43,5 @@ const ValidateGiftSelection = () => {
   });
 };
 ValidateGiftSelection();
+
+submitButton.addEventListener("click", validateForm());
